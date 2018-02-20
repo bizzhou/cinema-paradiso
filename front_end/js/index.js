a695f39c2dd523e2645c663c4c38038e3a87a8af
melanie0926 <melanieeelin@gmail.com>
2018-02-19 21:56:41 -0500

    modified home page

--- front_end/js/index.js
+++ front_end/js/index.js
@@ -11,22 +11,22 @@ $( document ).ready(function() {
         }).appendTo(imageContainers[i]);
 		
 		// add film name and rating
-		titles[i].innerHTML = filmNames[y];
+		// titles[i].innerHTML = filmNames[y];
 		var span = $('<span> 4.9/5.0</span>').appendTo(titles[i]);
 		
 		i++; 
 		y++;
-		if (y == 8) y = 0;	
+		if (y == 4) y = 0;	
 	}
 
 });
 
 var filmNames = ["Blade Runner 2049", "Coco", "Call Me By Your Name", "Lady Bird", "Get Out", "Dunkirk", "In the Fade", "Phantom Thread"];
 
-var images = ["https://images-na.ssl-images-amazon.com/images/M/MV5BMTU5NTkwNTA2NF5BMl5BanBnXkFtZTgwMjQ5MjY2MzI@.jpg"	,
-							"https://images-na.ssl-images-amazon.com/images/M/MV5BMTgyMTA2ODMxNl5BMl5BanBnXkFtZTgwMzEzMjMyNDM@.jpg",
-							"https://images-na.ssl-images-amazon.com/images/M/MV5BNzQxNDAwMTA2Ml5BMl5BanBnXkFtZTgwNzE0ODczMzI@.jpg",
-							"https://images-na.ssl-images-amazon.com/images/M/MV5BMDRlODhjN2YtYTNiMC00MzE3LWE5YzUtZGNiY2NmNTM4YzgxXkEyXkFqcGdeQXVyNjUwNzk3NDc@.jpg",
+var images = ["https://images-na.ssl-images-amazon.com/images/M/MV5BNzA1Njg4NzYxOV5BMl5BanBnXkFtZTgwODk5NjU3MzI@._V1_SX300.jpg"	,
+							"https://images-na.ssl-images-amazon.com/images/M/MV5BYjQ5NjM0Y2YtNjZkNC00ZDhkLWJjMWItN2QyNzFkMDE3ZjAxXkEyXkFqcGdeQXVyODIxMzk5NjA@._V1_SX300.jpg",
+							"https://images-na.ssl-images-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg",
+							"https://images-na.ssl-images-amazon.com/images/M/MV5BODhkZGE0NDQtZDc0Zi00YmQ4LWJiNmUtYTY1OGM1ODRmNGVkXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
 							"https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxMjEzNzE1NF5BMl5BanBnXkFtZTgwNDYwNjUzMTI@.jpg",
 							"https://images-na.ssl-images-amazon.com/images/M/MV5BNjA4MzEzOTc0N15BMl5BanBnXkFtZTgwOTcyNDY4MjI@.jpg",
 							"https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwNDI5Njg2M15BMl5BanBnXkFtZTgwMzIyNTYxNDM@.jpg",
