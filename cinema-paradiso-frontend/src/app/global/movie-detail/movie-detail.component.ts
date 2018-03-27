c92127ee8e4835dfda6b720542cda5d568dd7817
melanie0926 <melanieeelin@gmail.com>
2018-03-27 19:04:23 -0400

    added ratings section

--- cinema-paradiso-frontend/src/app/global/movie-detail/movie-detail.component.ts
+++ cinema-paradiso-frontend/src/app/global/movie-detail/movie-detail.component.ts
@@ -4,7 +4,8 @@ import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.scss']
+  styleUrls: ['./movie-detail.component.scss'],
+
 })
 export class MovieDetailComponent implements OnInit {
 
@@ -15,6 +16,36 @@ export class MovieDetailComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    $(document).ready(function() {
+      $('.bar span').hide();
+      $('#bar-five').animate({
+        width: '100%'}, 1000);
+      $('#bar-four').animate({
+        width: '90%'}, 1000);
+      $('#bar-three').animate({
+        width: '60%'}, 1000);
+      $('#bar-two').animate({
+        width: '50%'}, 1000);
+      $('#bar-one').animate({
+        width: '20%'}, 1000);
+
+      $('#bar-ten').animate({
+        width: '100%'}, 1000);
+      $('#bar-nine').animate({
+        width: '90%'}, 1000);
+      $('#bar-eight').animate({
+        width: '60%'}, 1000);
+      $('#bar-seven').animate({
+        width: '50%'}, 1000);
+      $('#bar-six').animate({
+        width: '20%'}, 1000);
+
+      setTimeout(function() {
+        $('.bar span').fadeIn('slow');
+      }, 2000);
+
+    });
 
   }
 
