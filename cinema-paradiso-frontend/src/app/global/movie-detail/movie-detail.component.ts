a6264b7901941ed8f7e2482f9376dc3521adae61
melanie0926 <melanieeelin@gmail.com>
2018-04-08 11:09:49 -0400

    movie detail: js to ts

--- cinema-paradiso-frontend/src/app/global/movie-detail/movie-detail.component.ts
+++ cinema-paradiso-frontend/src/app/global/movie-detail/movie-detail.component.ts
@@ -16,38 +16,36 @@ export class MovieDetailComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    $(document).ready(function() {
-      $('.bar span').hide();
-      $('#bar-five').animate({
-        width: '100%'}, 1000);
-      $('#bar-four').animate({
-        width: '90%'}, 1000);
-      $('#bar-three').animate({
-        width: '60%'}, 1000);
-      $('#bar-two').animate({
-        width: '50%'}, 1000);
-      $('#bar-one').animate({
-        width: '20%'}, 1000);
-
-      $('#bar-ten').animate({
-        width: '100%'}, 1000);
-      $('#bar-nine').animate({
-        width: '90%'}, 1000);
-      $('#bar-eight').animate({
-        width: '60%'}, 1000);
-      $('#bar-seven').animate({
-        width: '50%'}, 1000);
-      $('#bar-six').animate({
-        width: '20%'}, 1000);
-
-      setTimeout(function() {
-        $('.bar span').fadeIn('slow');
-      }, 1000);
-
-    });
-
+    this.ratingAnimation();
   }
+
+  ratingAnimation(): void {
+    $('.bar span').hide();
+    $('#bar-five').animate({
+      width: '100%'}, 1000);
+    $('#bar-four').animate({
+      width: '90%'}, 1000);
+    $('#bar-three').animate({
+      width: '60%'}, 1000);
+    $('#bar-two').animate({
+      width: '50%'}, 1000);
+    $('#bar-one').animate({
+      width: '20%'}, 1000);
+
+    $('#bar-ten').animate({
+      width: '100%'}, 1000);
+    $('#bar-nine').animate({
+      width: '90%'}, 1000);
+    $('#bar-eight').animate({
+      width: '60%'}, 1000);
+    $('#bar-seven').animate({
+      width: '50%'}, 1000);
+    $('#bar-six').animate({
+      width: '20%'}, 1000);
+
+    setTimeout(function() {
+      $('.bar span').fadeIn('slow');
+    }, 1000);
 
 
 }
