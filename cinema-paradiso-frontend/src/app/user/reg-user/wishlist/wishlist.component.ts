be5ed1e44dde1d7f7c2afa7ebd3597fa467d7dec
melanie0926 <melanieeelin@gmail.com>
2018-04-20 16:02:48 -0400

    merge

---
+++ cinema-paradiso-frontend/src/app/user/reg-user/wishlist/wishlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wishlist',
+  templateUrl: './wishlist.component.html',
+  styleUrls: ['./../reg-user.component.scss', './wishlist.component.scss']
+})
+export class WishlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
