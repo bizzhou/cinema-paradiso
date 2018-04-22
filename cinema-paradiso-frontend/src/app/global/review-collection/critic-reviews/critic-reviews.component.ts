f8ed84fa358d36e019b84d4067fd10fa14c146ec
melanie0926 <melanieeelin@gmail.com>
2018-04-22 12:45:43 -0400

    FE: separate critic review page and reg user review page

---
+++ cinema-paradiso-frontend/src/app/global/review-collection/critic-reviews/critic-reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-critic-reviews',
+  templateUrl: './critic-reviews.component.html',
+  styleUrls: ['./critic-reviews.component.scss']
+})
+export class CriticReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
