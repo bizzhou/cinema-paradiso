e7872e05705cee99859f977bdd8ed46d37998152
melanie0926 <melanieeelin@gmail.com>
2018-04-22 16:16:23 -0400

    updated style on movie detail/portfolio

--- cinema-paradiso-frontend/src/app/global/public-portfolio/reg-user/reg-user.component.ts
+++ cinema-paradiso-frontend/src/app/global/public-portfolio/reg-user/reg-user.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reg-user-portfolio',
   templateUrl: './reg-user.component.html',
-  styleUrls: ['./reg-user.component.scss']
+  styleUrls: ['../../movie-detail/md-review/md-review.component.scss', '../critic/critic.component.scss', './reg-user.component.scss']
 })
 export class RegUserPortfolioComponent implements OnInit {
 
