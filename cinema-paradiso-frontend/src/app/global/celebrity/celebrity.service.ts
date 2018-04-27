c280b4111e980ab7bb5a6a5ff71291dc5e499a22
Unknown <bin.zhou.1@stonybrook.edu>
2018-04-26 21:42:15 -0400

    FE: shrink photo size to improve load time..

--- cinema-paradiso-frontend/src/app/global/celebrity/celebrity.service.ts
+++ cinema-paradiso-frontend/src/app/global/celebrity/celebrity.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {AppConstant} from '../../app.constant';
 
-const CELEBRITY_SERVER = AppConstant.API_ENDPOINT + 'celebrity/';
+const CELEBRITY_SERVER = AppConstant.API_ENDPOINT + 'celebrity';
 
 @Injectable()
 export class CelebrityService {
@@ -10,6 +10,10 @@ export class CelebrityService {
   constructor(private http: HttpClient) { }
 
   getCelebirty(celebrityId: string) {
-    return this.http.get(CELEBRITY_SERVER + 'get/' + celebrityId);
+    return this.http.get(CELEBRITY_SERVER + `/get/${celebrityId}`);
+  }
+
+  getCelebrityFilmography(celebrityId: string) {
+    return this.http.get(CELEBRITY_SERVER + `/get/${celebrityId}/filmography`);
   }
 }
